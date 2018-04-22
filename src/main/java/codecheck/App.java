267a205e735833c56ca71ba1161d3a6054ed466c
Keita Suzuki <ksuzuki@cyber.t.u-tokyo.ac.jp>
2018-04-22 23:32:35 +0900

    first commit

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,9 +2,10 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
+		if (args.length != 1) {
+			throw new IllegalArgumentException();
 		}
+
+		System.out.println("Hello " + args[0] + "!");
 	}
 }
